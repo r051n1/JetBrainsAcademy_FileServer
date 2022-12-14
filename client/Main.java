fd3a554382f3550fa8675644cf85b65150c74b17
r051n1 <118889820+r051n1@users.noreply.github.com>
2022-12-14 12:29:53 +0100

    Add files via upload

--- client/Main.java
+++ client/Main.java
@@ -2,7 +2,6 @@ package client;
 
 public class Main {
     public static void main(String[] args) {
-        Client client = new Client();
-        client.start();
+        new Client().start();
     }
 }
